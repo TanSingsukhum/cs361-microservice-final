f23206b53feed80331a3b6ba21c8a07a3d8d7189
TanSingsukhum <tsings02642@gmail.com>
2024-02-06 15:06:59 -0800

    push

--- src/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const quizController = require('../controllers/quizController');
-
-const router = express.Router();
-
-// Endpoint to get a random question
-router.get('/quiz/:category/:difficulty', quizController.getQuizQuestion);
-
-module.exports = router;
