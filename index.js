34c62929d0890ba1adf290598c8559c07fae8528
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2024-02-06 16:36:31 -0800

    fix

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const cors = require('cors');
 const quizRoutes = require('./src/routes/quizRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 3000;
+
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use(express.json());
 app.use('/api', quizRoutes);
-
-//enable cors
-app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
